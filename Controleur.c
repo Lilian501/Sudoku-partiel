a376a4c1d8d0cb890f5dca6e482017a3cad0dd46
Lilian501 <130546845+Lilian501@users.noreply.github.com>
2023-05-09 16:18:59 +0200

    Update Controleur.c

--- Controleur.c
+++ Controleur.c
@@ -1,1 +1,18 @@
+int choix (char tableau[],int case, int nombre)
+{
+  /* Cette fonction prend en parametre le tableau du sudoku, la case et le nombre choisit par le joueur et modifie la tableau  */
+  
+}
 
+int scan(char tableau[], int case, int nombre)
+  
+{
+  /* Cette fonction prend en parametre le tableau du sudoku, la case et le nombre choisit par le joueur et vérifie qu'il peut être posé à cette endroit, cette fonction sera appeler dans la fonction choix    */
+}
+
+int victoire(char tableau[])
+{
+  /* Cette fonction prend en parametre le tableau du sudoku et vérifie si toute les cases sont remplit si c'est la cas le joueur à gagné.   */
+  
+  
+}
